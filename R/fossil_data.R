cdb7525b358460912b4964fe9b8fa312ec63c161
LauraBoyall <laura.boyall.2016@live.rhul.ac.uk>
2024-10-07 16:32:44 +0100

    update

---
+++ R/fossil_data.R
@@ -0,0 +1,23 @@
+#' Fossil Data
+#'
+#' A dataset containing fossil records for the reconstruction model.
+#'
+#' @docType data
+#' @name fossil_data
+#' @format A data frame with 512 rows and 12 columns:
+#' \describe{
+#'   \item{Age_BP}{Calibrated age in years BP}
+#'   \item{Si}{Si data}
+#'   \item{S}{S data}
+#'   \item{K}{K data}
+#'   \item{Ca}{Ca data}
+#'   \item{Ti}{Ti data}
+#'   \item{V}{V data}
+#'   \item{Mn}{Mn data}
+#'   \item{Fe}{Fe data}
+#'   \item{Rb}{Rb data}
+#'   \item{Sr}{Sr data}
+#'   \item{Zr}{Zr data}
+#' }
+#' @source Boyall et al (2024, JQS)
+"fossil_data"
---
+++ R/modern_data.R
@@ -0,0 +1,24 @@
+#' Modern Data
+#'
+#' A dataset containing modern records for the calibration model.
+#'
+#' @docType data
+#' @name modern_data
+#' @format A data frame with 199 rows and 13 columns:
+#' \describe{
+#'   \item{Age_BP}{Calibrated age in years BP}
+#'   \item{Annual}{Annual temperature anomalies}
+#'   \item{Si}{Si data}
+#'   \item{S}{S data}
+#'   \item{K}{K data}
+#'   \item{Ca}{Ca data}
+#'   \item{Ti}{Ti data}
+#'   \item{V}{V data}
+#'   \item{Mn}{Mn data}
+#'   \item{Fe}{Fe data}
+#'   \item{Rb}{Rb data}
+#'   \item{Sr}{Sr data}
+#'   \item{Zr}{Zr data}
+#' }
+#' @source Boyall et al (2024, JQS)
+"modern_data"
