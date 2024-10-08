98ce90c363ebef2d06296d399331e2b5af739b2a
LauraBoyall <laura.boyall.2016@live.rhul.ac.uk>
2024-10-08 09:46:34 +0100

    Fix lazy-load database issue and update package

--- R/SCUBIDO_cal.sorted.R
+++ R/SCUBIDO_cal.sorted.R
@@ -1,6 +1,6 @@
 #' Calculate the modern relationship between the proxy and climate
 #'
-#' The SCUBIDO_cal function uses the modern data from the \code{\link{SCUBIDO_input}}
+#' The `SCUBIDO_cal` function uses the modern data from the \code{\link{SCUBIDO_input}}
 #'     function and computes the relationship between the modern XRF data and an
 #'     instrumental climate time series. The function uses the JAGS package to fit
 #'     a multivariate polynomial regression model to identify the relationship
@@ -9,10 +9,9 @@
 #'     the elements and climate is then expressed through a likelihood function.
 #'     If check_convergence = TRUE a plot showing the Rhat values is produced. This
 #'     checks whether the Marcov Chain Monte Carlo (MCMC) algorithm has fitted.
-#'     If a point is <1.05 then is is assumed that the model has converged well.
+#'     If a point is <1.05 then it is assumed that the model has converged well.
 #'     The saved results from this function will be used to form the final reconstruction
 #'     in the \code{\link{SCUBIDO_reconstruct}} function.
-#'
 #'
 #' @param sorted the modern dataset saved after using the \code{\link{SCUBIDO_input}} function
 #' @param plot returns a plot of the relationship between the modern XRF elements and climate
@@ -23,15 +22,15 @@
 #'
 #' @examples
 #' \dontrun{
-#' SCUBIDO_cal(x,  plot = TRUE, summary = TRUE)
+#' SCUBIDO_cal(x, plot = TRUE, summary = TRUE)
 #' }
-#'
-
-SCUBIDO_cal<- function(sorted,  plot = TRUE, summary = TRUE){
+SCUBIDO_cal <- function(sorted, plot = TRUE, summary = TRUE) {
   UseMethod("SCUBIDO_cal")
 }
-#' @export
-SCUBIDO_cal.sorted <- function(sorted,  plot = TRUE, summary = TRUE) {
+
+# Do not export this function!
+SCUBIDO_cal.sorted <- function(sorted, plot = TRUE, summary = TRUE) {
+  # Function body remains unchanged here
   cal_data <- list(
     N.rows_m = nrow(sorted$xrf_m),
     N.cols_m = ncol(sorted$xrf_m),
@@ -49,7 +48,6 @@ SCUBIDO_cal.sorted <- function(sorted,  plot = TRUE, summary = TRUE) {
     "sd_rw",
     "mean_m"
   )
-
 
   jags_file <- system.file("jags_models", "modern_MVN_polynomial_model.jags", package = "SCUBIDO")
 
@@ -70,7 +68,6 @@ SCUBIDO_cal.sorted <- function(sorted,  plot = TRUE, summary = TRUE) {
     sorted = sorted
   )
 
-
   if (plot) {
     par(mar=c(3,3,2,1), mgp=c(2,.7,0), tck=-.01,las=1)
     par(mfrow = c(7,2))
@@ -82,21 +79,19 @@ SCUBIDO_cal.sorted <- function(sorted,  plot = TRUE, summary = TRUE) {
            xlab = 'Temperature',
            ylab = 'XRF',
            main = colnames(cal_data$xrf_m)[i],
-           ylim = range(c(cal_data$xrf_m[,i], cal_data$xrf_m[,i]))) #from the for loop to the end of this, it is just plotting the NAO against the XRF, nothing to do with the model
+           ylim = range(c(cal_data$xrf_m[,i], cal_data$xrf_m[,i])))
 
       mean_pred = with(par_means, beta0[i] + beta1[i] * temp_grid + beta2[i] * (temp_grid^2))
-      lines(temp_grid, mean_pred) #Here we are putting the parameter means on each of the graphs.
+      lines(temp_grid, mean_pred)
 
       low_pred = with(par_means, beta0[i] + beta1[i] * temp_grid + beta2[i] * (temp_grid^2) - 2 * sqrt(solve(par_means$Sigma_inv)[i,i]) )
       lines(temp_grid, low_pred, lty = 2)
 
       high_pred = with(par_means, beta0[i] + beta1[i] * temp_grid + beta2[i] * (temp_grid^2) + 2 * sqrt(solve(par_means$Sigma_inv)[i,i]) )
       lines(temp_grid, high_pred, lty = 2)
-      #abline(h = xrf_f_resc[510,i], col = 'red')
     }
     par(mfrow = c(1,1))
   }
-
 
   if (summary) {
     print(cal_run)
