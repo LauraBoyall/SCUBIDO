dac23c9303559603f6cd9ed1c3f9eb0f7dfe7f83
LauraBoyall <laura.boyall.2016@live.rhul.ac.uk>
2024-10-17 13:41:58 +0100

    Add large files to .gitignore to prevent future tracking

--- R/globals.R
+++
@@ -1,6 +0,0 @@
-utils::globalVariables(c("result_list", "calibration_list",
-                         "time_f", "par", "lines", "MDP_list",
-                         "median", "quantile", "yBP", "temp_low_95",
-                         "temp_high_95","temp_low_50", "temp_high_50",
-                         "temp_med", "MDP_list", "climate_reconstruction",
-                         "MDPs"))
