22dca46c69c4da224bbe43ab4039774457ef5ccc
LauraBoyall <laura.boyall.2016@live.rhul.ac.uk>
2024-10-17 12:20:53 +0100

    data documentation

--- data-raw/fossil_data.R
+++ data-raw/fossil_data.R
@@ -1,24 +1,23 @@
-#' Fossil Data
-#'
-#' A dataset containing fossil records for the reconstruction model.
+#' A dataset containing fossil records for the calibration model.
 #'
 #' @docType data
 #' @name fossil_data
 #' @export
-#' @format A data frame with 512 rows and 12 columns:
+#' @format A data frame with 570 rows and 14 columns:
 #' \describe{
-#'   \item{Age_BP}{Calibrated age in years BP}
-#'   \item{Si}{Si data}
-#'   \item{S}{S data}
-#'   \item{K}{K data}
-#'   \item{Ca}{Ca data}
-#'   \item{Ti}{Ti data}
-#'   \item{V}{V data}
-#'   \item{Mn}{Mn data}
-#'   \item{Fe}{Fe data}
-#'   \item{Rb}{Rb data}
-#'   \item{Sr}{Sr data}
-#'   \item{Zr}{Zr data}
+#'   \item{Age.BP}{Calibrated age in years BP}
+#'   \item{Al.clr}{Aluminium  (clr-transformed)}
+#'   \item{Si.clr}{Silicon (clr-transformed)}
+#'   \item{K.clr}{Potassium (clr-transformed)}
+#'   \item{Ca.clr}{Calcium  (clr-transformed)}
+#'   \item{Ti.clr}{Titanium (clr-transformed)}
+#'   \item{V.clr}{Vanadium (clr-transformed)}
+#'   \item{Cr.clr}{Chromium (clr-transformed)}
+#'   \item{Mn.clr}{Manganese (clr-transformed)}
+#'   \item{Fe.clr}{Iron (clr-transformed)}
+#'   \item{Zn.clr}{Zinc (clr-transformed)}
+#'   \item{Rb.clr}{Rubidium (clr-transformed)}
+#'   \item{Sr.clr}{Strontium (clr-transformed)}
+#'   \item{Zr.clr}{Zirconium (clr-transformed)}
 #' }
-#' @source Boyall et al (2024, JQS)
 "fossil_data"
